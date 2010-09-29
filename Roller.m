396c46745e785f4551d04383edd4763aab762219
Eric Saxby <sax@litp.org>
2010-09-29 09:32:07 -0700

    Update aughts to use 3/2/1/1 on 1/2/3/4 results

--- Roller.m
+++ Roller.m
@@ -42,9 +42,22 @@
 		
 		result = division;
 		
-		if (randomNumber < 5) {
-			result += (5 - randomNumber);
-		}		
+		switch (randomNumber) {
+			case 1:
+				result += 3;
+				break;
+			case 2:
+				result += 2;
+				break;
+			case 3:
+				result += 1;
+				break;
+			case 4:
+				result += 1;
+				break;
+			default:
+				break;
+		}
 	} else {
 		result = randomNumber;
 	}
